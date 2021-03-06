98e35ac5941183dc298d2f65d2238106e30e5f91
André Nas <andrenascontato@gmail.com>
2021-03-06 11:49:12 -0300

    product pronto

--- project/src/App.js
+++ project/src/App.js
@@ -2,12 +2,14 @@ import React from  'react'
 import Routes from  './routes'
 // npm install --save yarreact-router-hash-link
 import Menu from './components/menu'
+import Product from './components/product'
 
 function App() {
   return (
     <>
       <Menu/>
       <Routes/>
+      <Product/>
     </>
   );
 }
--- project/src/routes.js
+++ project/src/routes.js
@@ -3,12 +3,14 @@ import react from  'react'
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
 /* components */
 import Header from './components/header/Header'
+import Product from './components/product'
 
 export default function Routes(){
     return(
     <BrowserRouter>
         <Switch>
-            <Route path="/" component={Header}/>
+            <Route exact path="/" component={Header}/>
+            <Route path="/" component={Product}/>
         </Switch>
     </BrowserRouter>
     );
